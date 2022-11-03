34970a2f4e401c98cad25c20f751226f10f100d0
Alwoch <salwoch@gmail.com>
2022-11-03 11:55:18 +0300

    dashboard: implement new design

--- src/components/Dashboard/Card.js
+++ src/components/Dashboard/Card.js
@@ -1,18 +1,36 @@
-import "./Card.css";
+import "./Dashboard.css";
 
-const Card = ({children,description,price}) => {
+const Card = ({ title, icon, statistics, description }) => {
   return (
-   <div className=".container-sm dashboard">
-    <div className="card-content">
-     <div className="card-icon">
-     {children}
-     </div>
-     <div className="stats">
-      <h4>{description}</h4>
-      <h2>{price}</h2>
-     </div>
-    </div>
-   </div>
+    <>
+      <div className="card-content">
+        <div className="statsheader">
+          <div>
+            <h6 className="dark-text">{title}</h6>
+          </div>
+          <div className="align-self-end">
+            <i className={icon}></i>
+          </div>
+        </div>
+        <div>
+          <h2>{statistics}</h2>
+        </div>
+        <div>
+          <p className="brief">{description}</p>
+        </div>
+      </div>
+    </>
+    //  <div className=".container-sm dashboard">
+    //   <div className="card-content">
+    //    <div className="card-icon">
+    //    {children}
+    //    </div>
+    //    <div className="stats">
+    //     <h4>{description}</h4>
+    //     <h2>{price}</h2>
+    //    </div>
+    //   </div>
+    //  </div>
   );
 };
 
---
+++ src/components/Dashboard/Dashboarddata.js
@@ -0,0 +1,30 @@
+const ToolsData = [
+  {
+    id:1,
+    title:"Total number of tools",
+    icon:'fa-solid fa-toolbox',
+    statistics:"3700",
+    description:"5% more than last month"
+  },
+  {
+    id:2,
+    title:"Total edited tools",
+    icon:'fa-solid fa-pen-to-square',
+    statistics:"800",
+    description:"3% more than last month"
+  },{
+    id:3,
+    title:"Tools with missing information",
+    icon:'fa-solid fa-circle-question',
+    statistics:"2900",
+    description:"5% less than last month"
+  },{
+    id:4,
+    title:"Edited tools Vs missing information",
+    icon:"fa-solid fa-percent",
+    statistics:"15%",
+    description:"up 4 percentage points"
+  }
+];
+
+export default ToolsData;
